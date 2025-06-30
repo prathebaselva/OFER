8ceba3325ca8ba70b9895757f35d074e4c7fb74c
Pratheba Selvaraju <pselvaraju@ps004.is.localnet>
2025-06-30 12:31:40 +0200

    test script

--- test.sh
+++ test.sh
@@ -18,4 +18,4 @@
 module load cuda11/11.2.1
 
 
-python test.py --numcheckpoint 3 --cfg './src/configs/config_flameparamdiffusion_flame20.yml' --checkpoint1 'checkpoint/model_idrank.tar' --checkpoint2 'checkpoint/model_idgen_flame20.tar' --checkpoint3 'model_expgen_flame20.tar' --filename 'data/PICKPIK/validation/'${SLURM_ARRAY_TASK_ID}'.txt' --imagepath 'data/PICKPIK/' --outputpath 'output'
+python test.py --numcheckpoint 3 --cfg './src/configs/config_flameparamdiffusion_flame20.yml' --checkpoint1 'checkpoint/model_idrank.tar' --checkpoint2 'checkpoint/model_idgen_flame20.tar' --checkpoint3 'checkpoint/model_expgen_flame20.tar' --filename 'data/PICKPIK/validation/'${SLURM_ARRAY_TASK_ID}'.txt' --imagepath 'data/PICKPIK/' --outputpath 'output'
